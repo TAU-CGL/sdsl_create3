8c4b924efc2438b9dcf6827f67525fffacc9cc8f
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-08-06 15:09:51 +0300

    Create the first basic launchfile

---
+++ launch/launch.py
@@ -0,0 +1,8 @@
+from better_launch import BetterLaunch, launch_this
+
+@launch_this
+def create3_launch():
+    bl = BetterLaunch()
+
+    bl.include("create3_lidar_slam", "sensors_launch.py")
+    bl.include("create3_lidar_slam", "slam_toolbox_launch.py")
