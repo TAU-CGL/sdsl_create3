11a92da0ab3f7abedb0fe017edfb59d72f6cccef
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-08-06 15:45:52 +0300

    Comment out launchfile

--- launch/sdsl_create3_launch.py
+++ launch/sdsl_create3_launch.py
@@ -4,5 +4,5 @@ from better_launch import BetterLaunch, launch_this
 def create3_launch():
     bl = BetterLaunch()
 
-    bl.include("create3_lidar_slam", "sensors_launch.py")
-    bl.include("create3_lidar_slam", "slam_toolbox_launch.py")
+    # bl.include("create3_lidar_slam", "sensors_launch.py")
+    # bl.include("create3_lidar_slam", "slam_toolbox_launch.py")
